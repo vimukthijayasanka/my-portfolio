bdb8722b291dafe67f246b31617824f07dfba058
Vimukthi Jayasanka <jayasanka1997@gmail.com>
2025-03-01 21:10:43 +0530

    add a header

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,31 @@
 import $ from 'jquery';
 
-$('h1').css('color', 'crimson').css('font-family', 'Poppins,sans-serif');
+$(document).ready(function () {
+    $('#header').css({'visibility': 'visible', transform: "translateY(0)"});
+});
+
+$('.bi-facebook').on('click', function () {
+    window.open('https://web.facebook.com/vimukthi.jayasanka.16/','_blank');
+});
+
+$('.bi-linkedin').on('click', function () {
+    window.open('https://www.linkedin.com/in/vimukthijayasanka/','_blank');
+});
+
+$('.bi-whatsapp').on('click', function () {
+    const phoneNumber = '+94763651572';
+    const message = encodeURIComponent("Hello! I'm interested in your services."); // Pre-filled message
+    const whatsappURL = "https://wa.me/" + phoneNumber + "?text=" + message;
+
+    window.open(whatsappURL, '_blank');
+});
+
+$('.bi-envelope').on('click', function () {
+    const recipient = "jayasanka1997@email.com";
+    const subject = encodeURIComponent("Interested in your services");
+    const body = encodeURIComponent("Hello,\n\nI would like to know more about your services.");
+
+    const gmailURL = "https://mail.google.com/mail/?view=cm&fs=1&to=" + recipient + "&su=" + subject + "&body=" + body;
+
+    window.open(gmailURL, '_blank');
+});
